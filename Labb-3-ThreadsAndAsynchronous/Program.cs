4f568cdffd74644a3ca5ddb95823c23010f64efb
Theo Esberg <esbergtheo@gmail.com>
2023-04-26 13:59:26 +0200

    Add project files.

---
+++ Labb-3-ThreadsAndAsynchronous/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Labb_3_ThreadsAndAsynchronous
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
