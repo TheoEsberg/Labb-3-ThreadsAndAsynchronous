d01afbfc3fcea4c01c6b15ad8d828bdcb0bee60a
Theo Esberg <esbergtheo@gmail.com>
2023-04-27 12:09:47 +0200

    Fixes and Fixes and more Fixes

---
+++ Labb-3-ThreadsAndAsynchronous/Models/Car.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Spectre.Console;
+
+namespace Labb_3_ThreadsAndAsynchronous.Models
+{
+    internal class Car
+    {
+        public string Name { get; set; }
+        public double CurrentDistance { get; set; }
+        public double CurrentSpeed { get; set; }
+        public int CurrentPlacement { get; set; }
+        public int SleepTime { get; set; }
+
+        public static int Placement = 0;
+
+        public Car(string name, double currentDistance, double currentSpeed)
+        {
+            Name = name;
+            CurrentDistance = currentDistance;
+            CurrentSpeed = currentSpeed;
+        }
+
+        public int UpdatePlacement(double distanceToRace)
+        {
+            Placement++;
+            CurrentPlacement = Placement;
+            return CurrentPlacement;
+        }
+
+        public void RandomAction()
+        {
+            Random random = new Random();
+            int probability = random.Next(1, 51);
+            if (probability == 1) { NoFuel(); }
+            else if (probability <= 2) { FlatTire(); }
+            else if (probability <= 5) { HitBird(); }
+            else if (probability <= 10) { EngineTuning(); }
+            else if (probability <= 20) { EngineFailure(); }
+        }
+
+        public void NoFuel()
+        {
+            AnsiConsole.MarkupLine("[red]\n\t\b{0} needs to refuel, this will take 30 seconds to fix![/]", Name);
+            SleepTime = 30000;
+        }
+
+        public void FlatTire()
+        {
+            AnsiConsole.MarkupLine("[red]\n\t\b{0} got a flat tire, this will take 20 seconds to fix![/]", Name);
+            SleepTime = 20000;
+        }
+
+        public void HitBird()
+        {
+            AnsiConsole.MarkupLine("[red]\n\t\b{0} hit a bird and cant see out the window, this will take 10 seconds to fix![/]", Name);
+            SleepTime = 10000;
+        }
+
+        public void EngineFailure()
+        {
+            AnsiConsole.MarkupLine("[red]\n\t\b{0} got engine failure, the car runs 5km/h slower![/]", Name);
+            CurrentSpeed -= 5;
+        }
+
+        public void EngineTuning()
+        {
+            AnsiConsole.MarkupLine("[green]\n\t\b{0}'s team will do a engine tuning, the car runs 50km/h faster, 10 seconds delay![/]", Name);
+            SleepTime = 10000;
+            CurrentSpeed += 50;
+        }
+
+    }
+}
--- Labb-3-ThreadsAndAsynchronous/Program.cs
+++ Labb-3-ThreadsAndAsynchronous/Program.cs
@@ -1,10 +1,110 @@
-﻿namespace Labb_3_ThreadsAndAsynchronous
+﻿using Labb_3_ThreadsAndAsynchronous.Models;
+using System.Runtime.CompilerServices;
+using Spectre.Console;
+using static System.Net.Mime.MediaTypeNames;
+using System.Diagnostics;
+
+namespace Labb_3_ThreadsAndAsynchronous
 {
     internal class Program
     {
+        public static double DistanceToRace = 10;
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            //Console.SetOut(new StreamWriter(Console.OpenStandardOutput()) { AutoFlush = true });
+            Stopwatch stopwatch = Stopwatch.StartNew();
+            stopwatch.Start();
+
+            List<Car> cars = new List<Car> {
+                new Car("BMW", 0, 120),
+                new Car("AUDI", 0, 120),
+                new Car("VOLVO", 0, 120),
+                new Car("VOlKSWAGEN", 0, 120),
+                new Car("SAAB", 0, 120)
+            };
+
+            AnsiConsole.Progress()
+                .Columns(new ProgressColumn[]
+                {
+                    new TaskDescriptionColumn(),    // Task description
+                    new ProgressBarColumn(),        // Progress bar
+                    new PercentageColumn(),         // Percentage
+                    //new RemainingTimeColumn(),      // Remaining time
+                    new SpinnerColumn(),            // Spinner
+                })
+                .AutoRefresh(false)
+                .Start(ctx =>
+                {
+                    var threads = new List<Thread>();
+
+                    foreach (var car in cars)
+                    {
+                        var task = ctx.AddTask($"[bold blue]{car.Name}[/]", new ProgressTaskSettings { MaxValue = DistanceToRace });
+                        threads.Add(new Thread(() => RaceCar(car, task, stopwatch)));
+                    }
+
+                    foreach (var thread in threads)
+                    {
+                        thread.Start();
+                    }
+
+                    while (cars.Any(d => d.CurrentDistance < DistanceToRace))
+                    {
+                        if (stopwatch.ElapsedMilliseconds >= 31000)
+                        {
+                            foreach (var car in cars.Where(d => d.CurrentDistance < DistanceToRace))
+                            {
+                                Console.Out.Flush();
+                                car.RandomAction();
+                                Console.Out.Flush();
+                            }
+
+                            stopwatch.Restart();
+                        }
+                        if ((stopwatch.ElapsedMilliseconds) % 100 == 0)
+                        {
+                            ctx.Refresh();
+                        }
+                    }
+
+                    foreach (var thread in threads)
+                    {
+                        thread.Join();
+                    }
+
+                    ctx.Refresh();
+                });
+
+            cars.Sort((c1, c2) => c1.CurrentPlacement.CompareTo(c2.CurrentPlacement));
+
+            foreach (var car in cars)
+            {
+                if (car.CurrentPlacement == 1) { AnsiConsole.Markup("[green]\t{0} took {1} place![/]\n", car.Name, car.CurrentPlacement); }
+                else if (car.CurrentPlacement == 2) { AnsiConsole.Markup("[yellow]\t{0} took {1} place![/]\n", car.Name, car.CurrentPlacement); }
+                else { AnsiConsole.Markup("\t[red]{0} took {1} place![/]\n", car.Name, car.CurrentPlacement); }
+            }
+
+            AnsiConsole.MarkupLine("[yellow underline]\n\tPress any key to exit![/]");
+            Console.ReadKey();
+        }
+
+        static void RaceCar(Car car, ProgressTask task, Stopwatch stopwatch)
+        {
+            while (car.CurrentDistance < DistanceToRace)
+            {
+                if(car.SleepTime != 0)
+                {
+                    Thread.Sleep(car.SleepTime);
+                    car.SleepTime = 0;
+                }
+
+                Thread.Sleep(1000);
+                car.CurrentDistance += car.CurrentSpeed / 3.6 / 1000;
+                task.Value = car.CurrentDistance;
+            }
+
+            car.UpdatePlacement(DistanceToRace);
         }
     }
 }
